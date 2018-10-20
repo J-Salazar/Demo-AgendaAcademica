3de746ba436af3cf6904a5e5d7cf640e4bae49d4
Jose <jose.salazar1@unmsm.edu.pe>
2018-10-20 03:19:22 -0500

    Removido laravel/cashier; Avance vistas

--- app/Orgnz.php
+++ app/Orgnz.php
@@ -5,12 +5,12 @@ namespace App;
 use App\Notifications\OrgnzResetPassword;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Laravel\Cashier\Billable;
+
 
 class Orgnz extends Authenticatable
 {
     use Notifiable;
-    use Billable;
+
 
     /**
      * The attributes that are mass assignable.
@@ -27,7 +27,7 @@ class Orgnz extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token','card_brand',
+        'password',
     ];
 
     /**
--- resources/views/admin/layout/auth.blade.php
+++ resources/views/admin/layout/auth.blade.php
@@ -5,6 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!--Plantilla AdminLTE-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- fullCalendar 2.2.5-->
+    <link rel="stylesheet" href="{{asset('templates/plugins/fullcalendar/fullcalendar.min.css')}}">
+    <link rel="stylesheet" href="{{asset('templates/plugins/fullcalendar/fullcalendar.print.css')}}" media="print">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('templates/dist/css/adminlte.min.css')}}">
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+    <!-- -->
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -21,65 +35,251 @@
     </script>
 </head>
 <body>
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
+    {{--<nav class="navbar navbar-default navbar-static-top">--}}
+        {{--<div class="container">--}}
+            {{--<div class="navbar-header">--}}
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+                {{--<!-- Collapsed Hamburger -->--}}
+                {{--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">--}}
+                    {{--<span class="sr-only">Toggle Navigation</span>--}}
+                    {{--<span class="icon-bar"></span>--}}
+                    {{--<span class="icon-bar"></span>--}}
+                    {{--<span class="icon-bar"></span>--}}
+                {{--</button>--}}
 
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/admin') }}">
-                    {{ config('app.name', 'Laravel Multi Auth Guard') }}: Admin
-                </a>
-            </div>
+                {{--<!-- Branding Image -->--}}
+                {{--<a class="navbar-brand" href="{{ url('/admin') }}">--}}
+                    {{--{{ config('app.name', 'Laravel Multi Auth Guard') }}: Admin--}}
+                {{--</a>--}}
+            {{--</div>--}}
 
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    &nbsp;
-                </ul>
+            {{--<div class="collapse navbar-collapse" id="app-navbar-collapse">--}}
+                {{--<!-- Left Side Of Navbar -->--}}
+                {{--<ul class="nav navbar-nav">--}}
+                    {{--&nbsp;--}}
+                {{--</ul>--}}
 
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/admin/login') }}">Login</a></li>
-                        <li><a href="{{ url('/admin/register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
+                {{--<!-- Right Side Of Navbar -->--}}
+                {{--<ul class="nav navbar-nav navbar-right">--}}
+                    {{--<!-- Authentication Links -->--}}
+                    {{--@if (Auth::guest())--}}
+                        {{--<li><a href="{{ url('/admin/login') }}">Login</a></li>--}}
+                        {{--<li><a href="{{ url('/admin/register') }}">Register</a></li>--}}
+                    {{--@else--}}
+                        {{--<li class="dropdown">--}}
+                            {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">--}}
+{{--                                {{ Auth::user()->name }} <span class="caret"></span>--}}
+                            {{--</a>--}}
 
-                            <ul class="dropdown-menu" role="menu">
-                                <li>
-                                    <a href="{{ url('/admin/logout') }}"
-                                        onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
+                            {{--<ul class="dropdown-menu" role="menu">--}}
+                                {{--<li>--}}
+                                    {{--<a href="{{ url('/admin/logout') }}"--}}
+                                        {{--onclick="event.preventDefault();--}}
+                                                 {{--document.getElementById('logout-form').submit();">--}}
+                                        {{--Logout--}}
+                                    {{--</a>--}}
 
-                                    <form id="logout-form" action="{{ url('/admin/logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-    </nav>
+                                    {{--<form id="logout-form" action="{{ url('/admin/logout') }}" method="POST" style="display: none;">--}}
+                                        {{--{{ csrf_field() }}--}}
+                                    {{--</form>--}}
+                                {{--</li>--}}
+                            {{--</ul>--}}
+                        {{--</li>--}}
+                    {{--@endif--}}
+                {{--</ul>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</nav>--}}
+
+
+
+
+
 
     @yield('content')
 
     <!-- Scripts -->
     <script src="/js/app.js"></script>
+    <script src="{{asset('templates/plugins/jquery/jquery.min.js')}}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{asset('templates/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+    <!-- Slimscroll -->
+    <script src="{{asset('templates/plugins/slimScroll/jquery.slimscroll.min.js')}}"></script>
+    <!-- FastClick -->
+    <script src="{{asset('templates/plugins/fastclick/fastclick.js')}}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{asset('templates/dist/js/adminlte.min.js')}}"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="{{asset('templates/dist/js/demo.js')}}"></script>
+    <!-- fullCalendar 2.2.5 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js"></script>
+    <script src="{{asset('templates/plugins/fullcalendar/fullcalendar.min.js')}}"></script>
+    <!-- Page specific script -->
+    <script>
+        $(function () {
+
+            /* initialize the external events
+             -----------------------------------------------------------------*/
+            function ini_events(ele) {
+                ele.each(function () {
+
+                    // create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
+                    // it doesn't need to have a start or end
+                    var eventObject = {
+                        title: $.trim($(this).text()) // use the element's text as the event title
+                    }
+
+                    // store the Event Object in the DOM element so we can get to it later
+                    $(this).data('eventObject', eventObject)
+
+                    // make the event draggable using jQuery UI
+                    $(this).draggable({
+                        zIndex        : 1070,
+                        revert        : true, // will cause the event to go back to its
+                        revertDuration: 0  //  original position after the drag
+                    })
+
+                })
+            }
+
+            ini_events($('#external-events div.external-event'))
+
+            /* initialize the calendar
+             -----------------------------------------------------------------*/
+            //Date for the calendar events (dummy data)
+            var date = new Date()
+            var d    = date.getDate(),
+                m    = date.getMonth(),
+                y    = date.getFullYear()
+            $('#calendar').fullCalendar({
+                header    : {
+                    left  : 'prev,next today',
+                    center: 'title',
+                    right : 'month,agendaWeek,agendaDay'
+                },
+                buttonText: {
+                    today: 'today',
+                    month: 'month',
+                    week : 'week',
+                    day  : 'day'
+                },
+                //Random default events
+                events    : [
+                    {
+                        title          : 'All Day Event',
+                        start          : new Date(y, m, 1),
+                        backgroundColor: '#f56954', //red
+                        borderColor    : '#f56954' //red
+                    },
+                    {
+                        title          : 'Long Event',
+                        start          : new Date(y, m, d - 5),
+                        end            : new Date(y, m, d - 2),
+                        backgroundColor: '#f39c12', //yellow
+                        borderColor    : '#f39c12' //yellow
+                    },
+                    {
+                        title          : 'Meeting',
+                        start          : new Date(y, m, d, 10, 30),
+                        allDay         : false,
+                        backgroundColor: '#0073b7', //Blue
+                        borderColor    : '#0073b7' //Blue
+                    },
+                    {
+                        title          : 'Lunch',
+                        start          : new Date(y, m, d, 12, 0),
+                        end            : new Date(y, m, d, 14, 0),
+                        allDay         : false,
+                        backgroundColor: '#00c0ef', //Info (aqua)
+                        borderColor    : '#00c0ef' //Info (aqua)
+                    },
+                    {
+                        title          : 'Birthday Party',
+                        start          : new Date(y, m, d + 1, 19, 0),
+                        end            : new Date(y, m, d + 1, 22, 30),
+                        allDay         : false,
+                        backgroundColor: '#00a65a', //Success (green)
+                        borderColor    : '#00a65a' //Success (green)
+                    },
+                    {
+                        title          : 'Click for Google',
+                        start          : new Date(y, m, 28),
+                        end            : new Date(y, m, 29),
+                        url            : 'http://google.com/',
+                        backgroundColor: '#3c8dbc', //Primary (light-blue)
+                        borderColor    : '#3c8dbc' //Primary (light-blue)
+                    }
+                ],
+                editable  : true,
+                droppable : true, // this allows things to be dropped onto the calendar !!!
+                drop      : function (date, allDay) { // this function is called when something is dropped
+
+                    // retrieve the dropped element's stored Event Object
+                    var originalEventObject = $(this).data('eventObject')
+
+                    // we need to copy it, so that multiple events don't have a reference to the same object
+                    var copiedEventObject = $.extend({}, originalEventObject)
+
+                    // assign it the date that was reported
+                    copiedEventObject.start           = date
+                    copiedEventObject.allDay          = allDay
+                    copiedEventObject.backgroundColor = $(this).css('background-color')
+                    copiedEventObject.borderColor     = $(this).css('border-color')
+
+                    // render the event on the calendar
+                    // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
+                    $('#calendar').fullCalendar('renderEvent', copiedEventObject, true)
+
+                    // is the "remove after drop" checkbox checked?
+                    if ($('#drop-remove').is(':checked')) {
+                        // if so, remove the element from the "Draggable Events" list
+                        $(this).remove()
+                    }
+
+                }
+            })
+
+            /* ADDING EVENTS */
+            var currColor = '#3c8dbc' //Red by default
+            //Color chooser button
+            var colorChooser = $('#color-chooser-btn')
+            $('#color-chooser > li > a').click(function (e) {
+                e.preventDefault()
+                //Save color
+                currColor = $(this).css('color')
+                //Add color effect to button
+                $('#add-new-event').css({
+                    'background-color': currColor,
+                    'border-color'    : currColor
+                })
+            })
+            $('#add-new-event').click(function (e) {
+                e.preventDefault()
+                //Get value and make sure it is not null
+                var val = $('#new-event').val()
+                if (val.length == 0) {
+                    return
+                }
+
+                //Create events
+                var event = $('<div />')
+                event.css({
+                    'background-color': currColor,
+                    'border-color'    : currColor,
+                    'color'           : '#fff'
+                }).addClass('external-event')
+                event.html(val)
+                $('#external-events').prepend(event)
+
+                //Add draggable funtionality
+                ini_events(event)
+
+                //Remove event from text input
+                $('#new-event').val('')
+            })
+        })
+    </script>
 </body>
 </html>
